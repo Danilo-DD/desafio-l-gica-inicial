7a2f8f3c5873e8ee0bd340f8e8c1d2c190fe8c0e
WoolFighter <159034599+WoolFighter@users.noreply.github.com>
2024-02-23 14:54:48 -0300

    challangeDuo.js

---
+++ challangeDuo.js
@@ -0,0 +1,55 @@
+//Desafio de projeto 2!
+
+//Função que recebe como parâmetro as vitórias e derrotas do player:
+
+let victory = 100
+let defeat= 25
+let level=
+
+
+totalPlays=recieved(100, 25)
+console.log("You played "+totalPlays+" games in your entire career.")
+console.log("Like this, you have "+victory+" victories and "+defeat+" defeats.")
+function recieved(victory, defeat){
+    let sum=victory+defeat
+    return sum
+}
+
+let rankeadasValue=dataValue(100,25)
+console.log("Your rankead balance is "+ rankeadasValue+".")
+function dataValue(victory,defeat){
+    let sub= victory-defeat
+    return sub
+}
+
+switch(rankeadasValue){
+    case rankeadasValue<10:
+        level="iron"
+            break
+            
+    case rankeadasValue>=11 && rankeadasValue<=20:
+        level="copper"
+            break
+         
+    case rankeadasValue>=21 && rankeadasValue<=50:
+        level="silver"
+            break
+        
+    case rankeadasValue>=51 && rankeadasValue<=80:
+        level="gold"
+            break
+            
+    case rankeadasValue>=81 && rankeadasValue<=90:
+        level="diamond"
+            break
+            
+    case rankeadasValue>=91 && rankeadasValue<=100:
+        level="legendary"
+            break
+            
+    case rankeadasValue>=101:
+        level="imortal"
+}
+
+console.log("The Hero has a balance of "+rankeadasValue+" victories,and is in the level "+level+"!")
+
